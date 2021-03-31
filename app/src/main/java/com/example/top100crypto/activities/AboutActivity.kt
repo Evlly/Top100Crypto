d5411c17eb111f907b87de3b27e1c5223eda74ac
Dmitrii Glushkov <glushkov.dimaz2000@yandex.ru>
2021-03-31 23:02:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/top100crypto/activities/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.top100crypto.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.top100crypto.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
