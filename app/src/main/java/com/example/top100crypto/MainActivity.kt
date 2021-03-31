d9e3449db1fb0a01ae5c894050db601687732e48
Dmitrii Glushkov <glushkov.dimaz2000@yandex.ru>
2021-03-31 23:43:53 +0700

    Added about activity: info about app

--- app/src/main/java/com/example/top100crypto/MainActivity.kt
+++ app/src/main/java/com/example/top100crypto/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.top100crypto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.top100crypto.activities.AboutActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
