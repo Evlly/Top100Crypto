bd89f0ebbfc006c8accf86e150ba6c8a7ab597fe
Dmitrii Glushkov <glushkov.dimaz2000@yandex.ru>
2021-04-01 02:38:23 +0700

    Added ad in 2 modules

--- app/src/main/java/com/example/top100crypto/MainActivity.kt
+++ app/src/main/java/com/example/top100crypto/MainActivity.kt
@@ -3,14 +3,51 @@ package com.example.top100crypto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.example.top100crypto.activities.AboutActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+    private var mInterstitialAd: InterstitialAd? = null
+    private final var TAG = "MainActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        InterstitialAd.load(this,
+            "ca-app-pub-3940256099942544/1033173712",
+            AdRequest.Builder().build(),
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    Log.d(TAG, adError.message)
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    Log.d(TAG, "Ad was loaded.")
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+    }
+
+    private fun showAd(){
+        if (mInterstitialAd != null){
+            mInterstitialAd?.show(this)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/example/top100crypto/activities/AboutActivity.kt
+++ app/src/main/java/com/example/top100crypto/activities/AboutActivity.kt
@@ -1,14 +1,30 @@
 package com.example.top100crypto.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.top100crypto.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class AboutActivity : AppCompatActivity() {
+
+    lateinit var buttonRateApp: Button
+    lateinit var  adView: AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        buttonRateApp = findViewById<Button>(R.id.buttonRateApp)
+        adView = findViewById<AdView>(R.id.adView)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        buttonRateApp.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 }
